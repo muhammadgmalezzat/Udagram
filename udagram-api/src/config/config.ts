80330ab36f3cf8ba46d56d8e87fd87cc7e486f87
muhammad gmal ezzat <59686905+muhammadgmalezzat@users.noreply.github.com>
2022-07-14 13:53:50 +0200

    create tables local

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,4 +1,5 @@
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
+
 dotenv.config();
 
 // ENV variables 
@@ -6,11 +7,13 @@ dotenv.config();
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
 
+
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   port: Number(process.env.PORT),
+  dbport: Number(process.env.DB_PORT),
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,12 +1,15 @@
 import { Sequelize } from "sequelize-typescript";
-import { config } from "./config/config";
 
+import { config } from "./config/config";
+import dotenv from "dotenv";
+
+dotenv.config();
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
-
+  port: config.dbport,
   dialect: "postgres",
   storage: ":memory:",
 });
